eaae41c222fe706922771dcd202c929cb7d3e5fa
ARauf Git <umiasad2017@gmail.com>
2020-05-05 05:42:02 -0400

    added actions and reducers files to app

--- src/App.js
+++ src/App.js
@@ -2,10 +2,15 @@ import React from 'react';
 import logo from './logo.svg';
 import Posts from './components/Posts';
 import PostForm from './components/PostForm'
+import { Provider } from 'react-redux';
+import Store from './Store';
 import './App.css';
+
 
 function App() {
   return (
+    //store hold the whole state tree of the apllication
+    <Provider Store={Store}>
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
@@ -14,6 +19,7 @@ function App() {
       <hr/>
       <Posts />
     </div>
+    </Provider>
   );
 }
 
